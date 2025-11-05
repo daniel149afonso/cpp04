2f8109e6d3cb6de42bfdd7b6025400ca594cc6c4
Daniel Afonso <daniel149afonso@users.noreply.github.com>
2025-11-05 18:29:42 +0100

    building wrong animal

--- ex00/includes/WrongAnimal.hpp
+++ ex00/includes/WrongAnimal.hpp
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongCat.hpp                                            :+:      :+:    :+:   */
+/*   WrongAnimal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/04 17:42:20 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/11/05 16:05:34 by daniel149af      ###   ########.fr       */
+/*   Created: 2025/11/04 17:42:18 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/11/05 14:55:17 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WRONG_CAT_HPP
-#define WRONG_CAT_HPP
-#include "Animal.hpp"
-#include "WrongAnimal.hpp"
+#ifndef WRONG_ANIMAL_HPP
+#define WRONG_ANIMAL_HPP
+#include <iostream>
 
-class WrongCat: public WrongAnimal
+class WrongAnimal
 {
+	protected:
+		std::string _type;
 	public:
-		WrongCat();
-		WrongCat(const WrongCat& other);
-		WrongCat &operator=(const WrongCat &other);
-		~WrongCat();
-	void makeSound() const;
+		WrongAnimal();
+		WrongAnimal(const WrongAnimal& other);
+		WrongAnimal& operator=(const WrongAnimal& other);
+		virtual ~WrongAnimal();
+		virtual void makeSound() const;
+
+		std::string getType() const;
 };
+
 #endif
